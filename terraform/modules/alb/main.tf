8d517ee76d14486ff78c45efd584456b275e486b
Omar Islam <omarshaalan0100@gmail.com>
2024-10-23 08:13:35 +0300

    Adding github workflow

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_security_group" "alb_sg" {
   name        = "alb_sg"
   description = "Allow inbound traffic to ALB"
@@ -8,6 +12,26 @@ resource "aws_security_group" "alb_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "instance_sg" {
+  name        = "instance_sg"
+  description = "Allow inbound traffic to instances"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.web_server_port
+    to_port     = var.web_server_port
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]  # Allow traffic from ALB
   }
 
   egress {
@@ -34,12 +58,22 @@ resource "aws_lb_target_group" "tg" {
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "instance"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    path                = "/health"  # Adjust this to your health check path
+    protocol            = "HTTP"
+    port                = var.web_server_port
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg_attachment" {
   for_each = toset(var.private_instance_ids)
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = each.key
+  target_id        = each.value  # Use each.value
   port             = var.web_server_port
 }
 
