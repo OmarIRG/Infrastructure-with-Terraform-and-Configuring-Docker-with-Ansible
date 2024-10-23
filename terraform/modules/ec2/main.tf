d61f65b07e17201f18c97c13232f1428a2cd1767
Omar Islam <omarshaalan0100@gmail.com>
2024-10-23 08:02:25 +0300

    Adding github workflow

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to your VPC CIDR block
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -51,6 +58,13 @@ resource "aws_security_group" "private_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to your VPC CIDR block
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Adjust this to your VPC CIDR block
   }
